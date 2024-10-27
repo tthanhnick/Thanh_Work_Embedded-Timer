699f8c1ab098975def06b928cd68b9fad7cf3f1a
Nguyen Tien Thanh <92678285+tthanhnick@users.noreply.github.com>
2024-10-27 14:04:07 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -4,46 +4,28 @@ volatile int counter = 0;
 
 int main(void)
 {	
-	
-	NVICTimer2Config();
-	NVICTimer3Config();
-	
-	ClockSetting();
-	LiptInitialization();
-	
-	Timer0Config();
-	//for( int i = 0; i <100000; i++);
-	
-	Timer2InterruptConfig();
-	Timer2Config();
-	
-	Timer3InterruptConfig();
-	Timer3Config();
+	SoscConfig();
+	PLLConfig();
+	ClkOutput();
 	
 	LEDConfig();
-	
+	SysTick();
 	while(1)
 	{
-		if(counter == 1)
+		if(counter == 5)
 		{
-			SetOrClearBit((unsigned int*) GPIOD_PDOR, 15u, CLEAR_BIT);
+			SetOrClearBit((unsigned int*) GPIOD_PDOR, 15u, SET_BIT);
 		}
-		if (counter == 2)
+		if (counter == 10)
 		{
-			SetOrClearBit((unsigned int*) GPIOD_PDOR, 15u, SET_BIT); 
+			SetOrClearBit((unsigned int*) GPIOD_PDOR, 15u, CLEAR_BIT); 
 			counter = 0;
 		}
 	}
 }
 
-void LPIT0_Ch2_IRQHandler (void)
+void SysTick_Handler(void) 
 {
-	//counter++;
-	SetOrClearBit((unsigned int*) LIPT_MSR, 2, SET_BIT);  // Channel 2 Timmer flag
-}
-
-void LPIT0_Ch3_IRQHandler (void)
-{
-	counter++;
-	SetOrClearBit((unsigned int*) LIPT_MSR, 3, SET_BIT);  // Channel 2 Timmer flag
+  counter++;
+	SetOrClearBit((unsigned int*) SYSTICK_CTRL, 16, SET_BIT);
 }
